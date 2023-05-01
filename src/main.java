fea4c92fa7abaa86c6a59517ba20197d8cbe64f9
levihoulton <levijhoulton@gmail.com>
2023-05-01 14:47:53 -0700

    week 2 coding challenge

--- src/main.java
+++ src/main.java
@@ -1,12 +1,15 @@
 import Challenges.Week1Revature.Week1Revature;
-
-import java.util.Arrays;
+import Challenges.Week2Revature.Week2Revature;
 
 public class main {
 
     public static void main(String[] args){
         //Weeks 1
-        System.out.println(Week1Revature.arrayOfMultiples(3,50));
+        int[] intArr = {5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243};
+        int[] intArr2 = {12, 90, 75};
+
+        System.out.println(Week1Revature.arrayOfMultiples(3,8));
+        System.out.println(Week2Revature.warOfNumbers(intArr2));
     }
 
 }
